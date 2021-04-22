c223218efe3fe11e91de7aa3e38819868e3e028d
nicholas-oduor <oduor5742@gmail.com>
2021-04-22 09:51:52 +0300

    initial commit

--- teziapp/urls.py
+++ teziapp/urls.py
@@ -4,11 +4,26 @@ from . import views
 from .views import *
 from django.urls import path
 
+userSignup=UserViewSet.as_view({
+    'get':'list',
+    'post':'create'
+})
+
+userLogin=UserViewSet.as_view({
+    'get':'list',
+    'post':'list'
+})
+
+userDetail=UserViewSet.as_view({
+    'get':'retrieve'
+})
+
 router = DefaultRouter()
 router.register(r'User', UserViewSet)
 
 urlpatterns =[
     path('', views.home, name='index'),
-path('auth/signup/', userSignup, name='user_signup'),
+    path('auth/signup/', userSignup, name='user_signup'),
     path('auth/login/', userLogin, name='user_login'),
+    path('users/<int:pk>/', userDetail, name='user-detail'),
 ]
