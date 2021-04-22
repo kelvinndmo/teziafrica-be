4f22ea94ee24a70d4a6a209b6a823705f084cb70
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-22 16:38:57 +0300

    ignore templates, views

--- teziapp/urls.py
+++ teziapp/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls import url
@@ -13,19 +12,3 @@ urlpatterns = [
         re_path('^login/',auth_views.LoginView.as_view(template_name='teziapp/login.html'),name='login'),
         re_path('^logout',auth_views.LogoutView.as_view(template_name='teziapp/logout.html'),name='logout'),
     ]
-=======
-from rest_framework.routers import DefaultRouter
-from django.urls import path,re_path,include
-from . import views
-from .views import *
-from django.urls import path
-
-router = DefaultRouter()
-router.register(r'User', UserViewSet)
-
-urlpatterns =[
-    path('', views.home, name='index'),
-path('auth/signup/', userSignup, name='user_signup'),
-    path('auth/login/', userLogin, name='user_login'),
-]
->>>>>>> 9d9c1824772b8166a10bfe6542bb4b4d795420e2
--- teziapp/views.py
+++ teziapp/views.py
@@ -1,93 +1,63 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from .forms import UserRegister, ProfileUpdate, UserUpdate
+# from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+# from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
+# from .forms import UserRegister, ProfileUpdate, UserUpdate
 
-def register(request):
+# def register(request):
 
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request,f"Created Successfully")
-            return redirect('login')
-    else:
-        form = UserRegister()
-    context = {
-        'form':form
-    }
-    return render(request,"register.html", context)
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request,f"Created Successfully")
+#             return redirect('login')
+#     else:
+#         form = UserRegister()
+#     context = {
+#         'form':form
+#     }
+#     return render(request,"register.html", context)
 
-@login_required
-def profile(request):
+# @login_required
+# def profile(request):
 
-    title = 'Profile'
-    if request.method == "POST":
-        u_form = UserUpdate(request.POST, instance=request.user)
-        p_form = ProfileUpdate(request.POST,request.FILES,instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request,f"Profile Successfully Updated !")
-            return redirect('profile')
-    else:
-        u_form = UserUpdate(instance=request.user)
-        p_form = ProfileUpdate(instance=request.user.profile)
-    context = {
-        'title':title,
-        'u_form':u_form,
-        'p_form':p_form 
-    }
-    return render(request,'profile.html', context)
-
-
+#     title = 'Profile'
+#     if request.method == "POST":
+#         u_form = UserUpdate(request.POST, instance=request.user)
+#         p_form = ProfileUpdate(request.POST,request.FILES,instance=request.user.profile)
+#         if u_form.is_valid() and p_form.is_valid():
+#             u_form.save()
+#             p_form.save()
+#             messages.success(request,f"Profile Successfully Updated !")
+#             return redirect('profile')
+#     else:
+#         u_form = UserUpdate(instance=request.user)
+#         p_form = ProfileUpdate(instance=request.user.profile)
+#     context = {
+#         'title':title,
+#         'u_form':u_form,
+#         'p_form':p_form 
+#     }
+#     return render(request,'profile.html', context)
 
 
-def login(request):
-
-    context = {
-        'title':title,
-        'form':form
-    }
-    return render(request,'./teziapp/login.html', context)
 
 
-def logout(request):
+# def login(request):
 
-    context = {
-        'title':title,
-        'form':form
-    }
-    return render(request,'./teziapp/logout.html', context)
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import permissions
-from .models import *
-from rest_framework import status
-from .permissions import IsAdminOrReadOnly
-from rest_framework.decorators import action, permission_classes as permission_decorator
-from rest_framework.permissions import AllowAny
-from django.contrib.auth import login
+#     context = {
+#         'title':title,
+#         'form':form
+#     }
+#     return render(request,'./teziapp/login.html', context)
 
-# Create your views here.
-class IsAssigned(permissions.BasePermission): 
-    """
-    Only person who assigned has permission
-    """
-    def has_object_permission(self, request, view, obj):
-		# check if user who launched request is object owner 
-        if obj.assigned_to == request.user: 
-            return True
-        return False
-class IsReadOnlyOrIsAuthenticated(permissions.BasePermission):
-    def has_permission(self, request, view):
-        authenticated = request.user.is_authenticated
-        if not authenticated:
-            if view.action == '/':
-                return True
-            else:
-                return False
-        else:
-            return True
+
+# def logout(request):
+
+#     context = {
+#         'title':title,
+#         'form':form
+#     }
+#     return render(request,'./teziapp/logout.html', context)
