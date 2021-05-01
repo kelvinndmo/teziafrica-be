c916bf5c448dff8fe02db1301b1c8182a5838e0c
Vincent-Juma <vincentjuma64@gmail.com>
2021-05-01 15:23:45 +0300

    create .env file and secret

--- teziafricabe/settings.py
+++ teziafricabe/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('6bde06fe226266f9ac72dd399ebf00c2a9256b90e57e14fe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
