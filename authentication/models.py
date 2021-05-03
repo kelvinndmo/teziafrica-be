ac0a1ccdef5a4722898fdbdad4d4d47d0126a2ce
nicholas-oduor <oduor5742@gmail.com>
2021-05-04 01:25:16 +0300

    modified permissions.py file in the utils directory

--- authentication/models.py
+++ authentication/models.py
@@ -13,9 +13,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     CUSTOMER = 1
     CLIENT = 2
     ADMIN = 3
+    GUEST = 4
+    QUIZ = 5
+    QUIZRESPONSE = 6
     ROLE_CHOICES = (
         (CUSTOMER, 'customer'),
         (CLIENT, 'client'),
         (ADMIN, 'admin'),
+        (GUEST, 'guest'),
+        (QUIZ, 'quiz'),
+        (QUIZRESPONSE, 'quizresponse'),
     )
 
