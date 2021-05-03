75161ee1feffe5686bbbb4b763e8331d348efda7
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2021-05-03 21:08:21 +0300

    created flow builder models

--- flowbuilder/models.py
+++ flowbuilder/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+
+class Quiz(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=255)
+
+  def __str__(self):
+    return '%s - %s' % (self.title, self.user)
+
+    
+
+class QuizResponses(models.Model):
+  quiz = models.ForeignKey(Quiz, null=True, default=None, on_delete=models.CASCADE)
+  reply = models.CharField(max_length=255, default=None)
+  author = models.ForeignKey(User, on_delete = models.CASCADE)
+  order = models.IntegerField()
+
+  class Meta:
+    unique_together=('quiz', 'order')
+
+  def __str__(self):
+    return '%s - %s' % (self.reply, self.author)
