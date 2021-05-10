3d8ce03d822cb769460b343c1b6982d4416b7b92
nicholas-oduor <oduor5742@gmail.com>
2021-05-10 10:40:47 +0300

    modified utils.py file

--- teziafricabe/settings.py
+++ teziafricabe/settings.py
@@ -110,27 +110,27 @@ WSGI_APPLICATION = 'teziafricabe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT')
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
 
 AUTH_USER_MODEL = 'authentication.User'
 DEFAULT_AUTO_FILED = 'django.db.models.BigAutoField'
--- utils/permissions.py
+++ utils/permissions.py
@@ -1,7 +1,26 @@
 # from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
+
 class IsTeziAdmin(BasePermission):
+
+    def has_object_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'AD'
+
+
+class Company(BasePermission):
+
+    def has_object_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'CO'
+
+
+class Staff(BasePermission):
+
+    def has_object_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'ST'
+
+
+class Client(BasePermission):
 
     def has_object_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'CL'
