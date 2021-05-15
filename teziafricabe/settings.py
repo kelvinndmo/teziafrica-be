ae69bbda427735af20502a79db84b2ae907e9c0e
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2021-05-15 08:52:18 +0300

    add missing serializer fields

--- teziafricabe/settings.py
+++ teziafricabe/settings.py
@@ -116,12 +116,12 @@ WSGI_APPLICATION = 'teziafricabe.wsgi.application'
 # Local Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Online Database
